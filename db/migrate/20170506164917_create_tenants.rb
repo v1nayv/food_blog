b2395a20dad4929d071af63b6298ae15c3491485
Vinay Vasudevamurthy <vinay.vas@gmail.com>
2017-05-06 12:20:02 -0500

    Initial commit.

---
+++ db/migrate/20170506164917_create_tenants.rb
@@ -0,0 +1,10 @@
+class CreateTenants < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tenants do |t|
+      t.string :name
+      t.string :domain
+
+      t.timestamps
+    end
+  end
+end
