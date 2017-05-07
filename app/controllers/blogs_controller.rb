fdb4a760a665dc89c9f0b4020108271f89d0069d
Vinay Vasudevamurthy <vinay.vas@gmail.com>
2017-05-07 14:34:59 -0500

    create and retrieve working.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,13 +1,21 @@
 class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
+
+    puts '***************'
+    puts @blogs.length
+    puts '***************'
   end
 
   def show
   end
 
   def create
-    puts params
+    form_params = params.require(:blog).permit(:name, :description)
+    @blog = Blog.new(form_params)
+    @blog.save
+
+    redirect_to action: "index"
   end
 
   def new
